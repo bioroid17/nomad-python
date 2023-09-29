04b6bb9fe715b03b0c1df37a81366946a3906637
bioroid17 <trace1997@ajou.ac.kr>
2023-09-30 02:22:37 +0900

    VARIABLES AND FUNCTIONS - Variables

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello world!")
+a = 2
+b = 3
+c = a + b
+a = 1
+b = 10
+print(c)
+
+# my age = 88 	# error
+myage = 88
+my_age = 88		# snake case <- Python style!
+myAge = 88		# camel case
+
+my_age = 1    # 정상
+# 123my_age = 1 # 에러
+# *my_age = 1   # 에러
+my_age123 = 1 # 정상
