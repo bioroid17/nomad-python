f404cd31016f0688a0a79417699bd5634deb8d1b
bioroid17 <trace1997@ajou.ac.kr>
2023-09-30 06:06:27 +0900

    Code Challenge - Simple Calculator

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
-def say_hello(user_name="anonymous"):
-	print("hello", user_name)
-	
-say_hello("nico")
-say_hello()
+def add(a=0, b=0):
+    print(a + b)
+
+def subtract(a=0, b=0):
+    print(a - b)
+
+def multiply(a=0, b=0):
+    print(a * b)
+
+def divide(a=0, b=1):
+    print(a / b)
+
+def power(a=0, b=1):
+    print(a ** b)
+
+add(55, 34)
+subtract(16, 21)
+multiply(6, 4)
+divide(4, 3)
+power(3, 3)
+
+add()
+subtract()
+multiply()
+divide()
+power()
