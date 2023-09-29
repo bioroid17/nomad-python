d0967d629237f324774735b4d88d7535e30f7412
bioroid17 <trace1997@ajou.ac.kr>
2023-09-30 02:38:52 +0900

    VARIABLES AND FUNCTIONS - Booleans and Strings

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-a = 2
-b = 3
-c = a + b
-a = 1
-b = 10
-print(c)
-
-# my age = 88 	# error
-myage = 88
-my_age = 88		# snake case <- Python style!
-myAge = 88		# camel case
-
-my_age = 1    # 정상
-# 123my_age = 1 # 에러
-# *my_age = 1   # 에러
-my_age123 = 1 # 정상
+# my_name = nico	# error
+my_name = "nico"	# text
+my_name = "1234"	# text
+my_name = 1234		# number
+my_name = True		# boolean
+my_name = False		# boolean
+my_name = "True"	# text
