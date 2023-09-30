9aba3877a2b602bc9453716551af0d70e1b2e70a
bioroid17 <trace1997@ajou.ac.kr>
2023-10-01 03:20:38 +0900

    CONTROL FLOW - Python Casino

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
-"""
 from random import randint, uniform
 
-user_choice = int(input("Choose number."))
-pc_choice = randint(1, 50)
+print("Welcome to Python Casino")
 
-if user_choice == pc_choice:
-	print("You won!")
-elif user_choice > pc_choice:
-	print("Lower! Computer chose", pc_choice)
-elif user_choice < pc_choice:
-	print("Higher! Computer chose", pc_choice)
-"""
+pc_choice = randint(1, 100)
 
-distance = 0
-
-while distance < 20:
-	print("I'm running:", distance, "km")
-	distance = distance + 1
+playing = True
+while playing:
+    user_choice = int(input("Choose number (1~100):"))
+    if user_choice == pc_choice:
+        print("You won!")
+        playing = False
+    elif user_choice > pc_choice:
+        print("Lower!")
+    elif user_choice < pc_choice:
+        print("Higher")
