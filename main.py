d7739785aeee091dc26adb3d63d6bd53728b8879
bioroid17 <trace1997@ajou.ac.kr>
2023-09-30 23:08:13 +0900

    CONTROL FLOW - Else & Elif

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-a = "nico"
+winner = 5
 
-if a == "nico":
-	print("Correct!")
+if winner > 10:
+	print("Winner is greater than 10")
+elif winner < 10:
+	print("Winner is less than 10")
+else:
+	print("Winner is 10")
