4974819b0770d86d8b4fe884247648d6f69497be
bioroid17 <trace1997@ajou.ac.kr>
2023-09-30 22:08:41 +0900

    VARIABLES AND FUNCTIONS - Return Recap

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-def tax_calc(money):
-	return money * 0.35
+def make_juice(fruit):
+	return f"{fruit}+🥤"
 
-def pay_tax(tax):
-	print("thank you for paying", tax)
+def add_ice(juice):
+	return f"{juice}+🧊"
 
-pay_tax(tax_calc(15000))
+def add_sugar(iced_juice):
+	return f"{iced_juice}+🍭"
+
+juice = make_juice("🍇")
+cold_juice = add_ice(juice)
+perfect_juice = add_sugar(cold_juice)
+
+print(perfect_juice)
