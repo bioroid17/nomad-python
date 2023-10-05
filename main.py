ce310e9665097b139f20efdcbbc19aa571c5779e
bioroid17 <trace1997@ajou.ac.kr>
2023-10-06 04:09:11 +0900

    DATA STRUCTURES - Dicts

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-days = ("Mon", "Tue", "Wed")
-
-print(days[-3])
+player = {
+    'name': 'nico',
+    'age': 12,
+    'alive': True,
+	'fav_food':["🍕", "🍔"]
+}
+player['fav_food'].append("🍜")
+print(player.get('fav_food'))
+print(player['fav_food'])
