24c8f2ab27252e7a1e1cf878f8d25299417ba8d8
bioroid17 <trace1997@ajou.ac.kr>
2023-10-06 05:11:40 +0900

    DATA STRUCTURES - Recap 1

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
+# print("nico".upper())
+# print("nico".endswith("a"))
+
+# numbers = [5, 3, 1, 5, 7, 3, "True", True, 12]
+# numbers.append(["🍕", "🍔"])
+# print(numbers[-1])
+
+# numbers = (1, 2, 3, 4, 5)
+
 player = {
-    'name': 'nico',
-    'age': 12,
-    'alive': True,
-	'fav_food':["🍕", "🍔"]
+    "name": "nico",
+    "age": 12,
+    "alive": True,
+    "fav_food": ("🍕", "🍔"),
+    "friend": {
+        "name": "lynn",
+        "fav_food": ["🍎"]
+    }
 }
-player['fav_food'].append("🍜")
-print(player.get('fav_food'))
-print(player['fav_food'])
+
+player["fav_food"] = "🍎"
+player.pop("alive")
+player["friend"]["fav_food"].append("🍌")
+print(player)
