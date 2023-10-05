e95eb4270fd1cca68d81533cbecc37413a57e4fa
bioroid17 <trace1997@ajou.ac.kr>
2023-10-06 03:15:52 +0900

    DATA STRUCTURES - Lists

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
-# days_of_week = ["Mon", "Tue", "Wed", "Thu", "Fri"]
+days_of_week = ["Mon", "Tue", "Wed", "Thu", "Fri"]
 
-name = "nico"
-print(name.endswith("o"))
+# print(days_of_week.count("Wed"))
+
+# days_of_week.clear()
+# print(days_of_week)
+
+# print(days_of_week)
+# days_of_week.reverse()
+# print(days_of_week)
+
+# print(days_of_week)
+# days_of_week.append("Sat")
+# print(days_of_week)
+# days_of_week.append("Sun")
+# print(days_of_week)
+
+# print(days_of_week)
+# days_of_week.remove("Fri")
+# print(days_of_week)
+
+print(days_of_week[2])
