e77d24a3fae457044689baba8a8ba4675d8ad738
bioroid17 <trace1997@ajou.ac.kr>
2023-10-07 05:26:06 +0900

    DATA STRUCTURES - Status Codes

--- main.py
+++ main.py
@@ -8,7 +8,15 @@ websites = (
     "https://tiktok.com"
 )
 
+results = {}
+
 for website in websites:
     if not website.startswith("https://"):
         website = f"https://{website}"
-    print(website)
+    response = get(website)
+    if response.status_code == 200:
+        results[website] = "OK"
+    else:
+        results[website] = "FAILED"
+
+print(results)
