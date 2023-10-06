3c2e4dc56274b3655fcf91883eb4f56fc39bf594
bioroid17 <trace1997@ajou.ac.kr>
2023-10-07 01:59:26 +0900

    DATA STRUCTURES - For Loops

--- main.py
+++ main.py
@@ -1,24 +1,10 @@
-# print("nico".upper())
-# print("nico".endswith("a"))
+websites = (
+    "google.com",
+    "airbnb.com",
+    "twitter.com",
+    "facebook.com",
+    "tiktok.com"
+)
 
-# numbers = [5, 3, 1, 5, 7, 3, "True", True, 12]
-# numbers.append(["🍕", "🍔"])
-# print(numbers[-1])
-
-# numbers = (1, 2, 3, 4, 5)
-
-player = {
-    "name": "nico",
-    "age": 12,
-    "alive": True,
-    "fav_food": ("🍕", "🍔"),
-    "friend": {
-        "name": "lynn",
-        "fav_food": ["🍎"]
-    }
-}
-
-player["fav_food"] = "🍎"
-player.pop("alive")
-player["friend"]["fav_food"].append("🍌")
-print(player)
+for website in websites:
+    print("potato is equals to", website)
