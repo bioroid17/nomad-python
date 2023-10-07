20345d47e917d336c193b88656e430383ec4e572
bioroid17 <trace1997@ajou.ac.kr>
2023-10-07 22:12:03 +0900

    JOB SCRAPPER - Initial Request

--- main.py
+++ main.py
@@ -1,36 +1,10 @@
 from requests import get
 
-websites = (
-    "google.com",
-    "airbnb.com",
-    "https://twitter.com",
-    "facebook.com",
-    "https://tiktok.com",
+base_url = "https://weworkremotely.com/remote-jobs/search?term="
+search_term = "python"
 
-    "https://httpstat.us/101",
-    "https://httpstat.us/200",
-    "https://httpstat.us/300",
-    "https://httpstat.us/404",
-    "https://httpstat.us/502",
-)
-
-results = {}
-
-for website in websites:
-    if not website.startswith("https://"):
-        website = f"https://{website}"
-    response = get(website)
-    if response.status_code >= 500:
-        results[website] = "Server error"
-    elif response.status_code >= 400:
-        results[website] = "Client error"
-    elif response.status_code >= 300:
-        results[website] = "Redirection"
-    elif response.status_code >= 200:
-        results[website] = "Successful"
-    elif response.status_code >= 100:
-        results[website] = "Informational"
-    else:
-        results[website] = "Unknown error"
-
-print(results)
+response = get(f"{base_url}{search_term}")
+if response.status_code != 200:
+    print("Can't request website")
+else:
+    print(response.text)
