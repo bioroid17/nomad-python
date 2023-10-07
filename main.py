a13d0dd3a1a725eab014194724f61a8d0f8e08bd
bioroid17 <trace1997@ajou.ac.kr>
2023-10-07 09:09:44 +0900

    Code Challenge - Status Codes

--- main.py
+++ main.py
@@ -5,7 +5,13 @@ websites = (
     "airbnb.com",
     "https://twitter.com",
     "facebook.com",
-    "https://tiktok.com"
+    "https://tiktok.com",
+
+    "https://httpstat.us/101",
+    "https://httpstat.us/200",
+    "https://httpstat.us/300",
+    "https://httpstat.us/404",
+    "https://httpstat.us/502",
 )
 
 results = {}
@@ -14,9 +20,17 @@ for website in websites:
     if not website.startswith("https://"):
         website = f"https://{website}"
     response = get(website)
-    if response.status_code == 200:
-        results[website] = "OK"
+    if response.status_code >= 500:
+        results[website] = "Server error"
+    elif response.status_code >= 400:
+        results[website] = "Client error"
+    elif response.status_code >= 300:
+        results[website] = "Redirection"
+    elif response.status_code >= 200:
+        results[website] = "Successful"
+    elif response.status_code >= 100:
+        results[website] = "Informational"
     else:
-        results[website] = "FAILED"
+        results[website] = "Unknown error"
 
 print(results)
