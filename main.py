e4f3b7e3e5ed32f356f25bd6990154adcec084e2
bioroid17 <trace1997@ajou.ac.kr>
2023-10-09 04:47:12 +0900

    JOB SCRAPPER - Indeed 403 Fix
    기존 강의에선 Indeed가 봇을 차단 안 했음. 지금은 차단하므로 이걸 먼저 수강함.

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-from requests import get
 from bs4 import BeautifulSoup
 from extractors.wwr import extract_wwr_jobs
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
-jobs = extract_wwr_jobs("python")
-print(jobs)
+base_url = "https://kr.indeed.com/jobs?q="
+search_term = "python"
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)  # 브라우저 꺼짐 방지 코드
+
+# 크롬드라이버를 최신으로 유지해줍니다.
+browser = webdriver.Chrome(service=Service(
+    ChromeDriverManager().install()), options=chrome_options)
+browser.get(f"{base_url}{search_term}")
+
+print(browser.page_source)
