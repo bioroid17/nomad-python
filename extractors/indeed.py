63eedf082d72de7e8fc790ed6aed3a5a74f06691
bioroid17 <trace1997@ajou.ac.kr>
2023-10-12 01:34:29 +0900

    JOB SCRAPPER - Refactor

---
+++ extractors/indeed.py
@@ -0,0 +1,71 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+def get_page_count(keyword):
+    base_url = "https://kr.indeed.com/jobs?q="
+    chrome_options = Options()
+    chrome_options.add_experimental_option("detach", True)  # 브라우저 꺼짐 방지 코드
+
+    # 크롬드라이버를 최신으로 유지해줍니다.
+    browser = webdriver.Chrome(service=Service(
+        ChromeDriverManager().install()), options=chrome_options)
+    browser.get(f"{base_url}{keyword}")
+
+    soup = BeautifulSoup(browser.page_source, "html.parser")
+    pagination = soup.select_one("nav.css-jbuxu0.ecydgvn0")
+    if pagination == None:
+        return 1
+    pages = pagination.find_all("div", recursive=False)
+    count = len(pages)
+    if count >= 5:
+        return 5
+    elif count <= 1:
+        return 1
+    else:
+        return count
+
+
+def extract_indeed_jobs(keyword):
+    pages = get_page_count(keyword)
+    print("Found", pages, "pages")
+    chrome_options = Options()
+    chrome_options.add_experimental_option("detach", True)  # 브라우저 꺼짐 방지 코드
+
+    # 크롬드라이버를 최신으로 유지해줍니다.
+    browser = webdriver.Chrome(service=Service(
+        ChromeDriverManager().install()), options=chrome_options)
+    results = []
+    for page in range(pages):
+        base_url = "https://kr.indeed.com/jobs"
+        final_url = f"{base_url}?q={keyword}&start={page*10}"
+        print("Requesting", final_url)
+        browser.get(final_url)
+
+        soup = BeautifulSoup(browser.page_source, "html.parser")
+        job_list = soup.find("ul", class_="css-zu9cdh")
+        jobs = job_list.find_all("li", recursive=False)
+        for job in jobs:
+            zone = job.find("div", class_="mosaic-zone")
+            if zone == None:
+                anchor = job.select_one("h2 a")
+                title = anchor['aria-label']
+                link = anchor['href']
+                company = job.find("span", class_="companyName")
+                location = job.find("div", class_="companyLocation")
+                job_data = {
+                    "link": f"https://kr.indeed.com{link}",
+                    "company": company.string,
+                    "location": location.string,
+                    "position": title
+                }
+                results.append(job_data)
+    return results
+
+
+jobs = extract_indeed_jobs("python")
+print(jobs)
+print(len(jobs))
