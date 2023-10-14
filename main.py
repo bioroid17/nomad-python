240c4604aee4dacde49cedbaa6f9250cd7235aa4
bioroid17 <trace1997@ajou.ac.kr>
2023-10-15 02:55:11 +0900

    🔥 (BONUS) BUILD A WEBSITE WITH FLASK 🔥 - Arguments

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from extractors.indeed import extract_indeed_jobs
+from extractors.wwr import extract_wwr_jobs
+from extractors.remoteok import extract_remoteok_jobs
 
 app = Flask("JobScrapper")
 
@@ -10,7 +13,12 @@ def home():
 
 @app.route("/search")
 def search():
-    return render_template("search.html", name="nico")
+    keyword = request.args.get("keyword")
+    indeed = extract_indeed_jobs(keyword)
+    wwr = extract_wwr_jobs(keyword)
+    remoteok = extract_remoteok_jobs(keyword)
+    jobs = indeed + wwr + remoteok
+    return render_template("search.html", keyword=keyword, jobs=jobs)
 
 
 app.run("0.0.0.0")
