7d23902cc35664da216139f4bc313857e7aabd0b
bioroid17 <trace1997@ajou.ac.kr>
2023-10-16 01:53:24 +0900

    🔥 (BONUS) BUILD A WEBSITE WITH FLASK 🔥 - File Download

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, send_file
 from extractors.indeed import extract_indeed_jobs
 from extractors.wwr import extract_wwr_jobs
 from extractors.remoteok import extract_remoteok_jobs
+from file import save_to_file
 
 app = Flask("JobScrapper")
 
@@ -16,6 +17,8 @@ def home():
 @app.route("/search")
 def search():
     keyword = request.args.get("keyword")
+    if keyword == None or keyword == "":
+        return redirect("/")
     if keyword in db:
         jobs = db[keyword]
     else:
@@ -25,6 +28,17 @@ def search():
         jobs = indeed + wwr + remoteok
         db[keyword] = jobs
     return render_template("search.html", keyword=keyword, jobs=jobs)
+
+
+@app.route("/export")
+def export():
+    keyword = request.args.get("keyword")
+    if keyword == None or keyword == "":
+        return redirect("/")
+    if keyword not in db:
+        return redirect(f"/search?keyword={keyword}")
+    save_to_file(keyword, db[keyword])
+    return send_file(f"{keyword}.csv", as_attachment=True)
 
 
 app.run("0.0.0.0")
