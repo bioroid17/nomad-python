5478ced72b7254834a42d62d07211f77d4e92773
bioroid17 <trace1997@ajou.ac.kr>
2024-06-30 00:47:49 +0900

    OOP - Why We Need OOP

--- main.py
+++ main.py
@@ -1,36 +1,21 @@
-from requests import get
+def create_player(name, xp, team):
+    return {
+        "name": name,
+        "XP": xp,
+        "team": team
+    }
 
-websites = (
-    "google.com",
-    "airbnb.com",
-    "https://twitter.com",
-    "facebook.com",
-    "https://tiktok.com",
 
-    "https://httpstat.us/101",
-    "https://httpstat.us/200",
-    "https://httpstat.us/300",
-    "https://httpstat.us/404",
-    "https://httpstat.us/502",
-)
+def introduce_player(player):
+    name = player["name"]
+    team = player["name"]
+    print(f"Hello! my name is {name} and I play for {team}")
 
-results = {}
 
-for website in websites:
-    if not website.startswith("https://"):
-        website = f"https://{website}"
-    response = get(website)
-    if response.status_code >= 500:
-        results[website] = "Server error"
-    elif response.status_code >= 400:
-        results[website] = "Client error"
-    elif response.status_code >= 300:
-        results[website] = "Redirection"
-    elif response.status_code >= 200:
-        results[website] = "Successful"
-    elif response.status_code >= 100:
-        results[website] = "Informational"
-    else:
-        results[website] = "Unknown error"
+nico = create_player("Nico", 1500, "Team X")
+lynn = create_player("Lynn", 1500, "Team Blue")
 
-print(results)
+teams = {
+    "Team X": [nico],
+    "Team Blue": [lynn]
+}
