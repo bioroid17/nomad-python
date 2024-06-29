5bb4cac83c7c78a6f10d88a2c9531578d4610583
bioroid17 <trace1997@ajou.ac.kr>
2024-06-30 01:46:40 +0900

    OOP - Classes

--- main.py
+++ main.py
@@ -1,21 +1,9 @@
-def create_player(name, xp, team):
-    return {
-        "name": name,
-        "XP": xp,
-        "team": team
-    }
+class Puppy:
+    def __init__(self):
+        self.name = "Ruffus"
+        self.age = 0.1
+        self.breed = "Beagle"
 
 
-def introduce_player(player):
-    name = player["name"]
-    team = player["name"]
-    print(f"Hello! my name is {name} and I play for {team}")
-
-
-nico = create_player("Nico", 1500, "Team X")
-lynn = create_player("Lynn", 1500, "Team Blue")
-
-teams = {
-    "Team X": [nico],
-    "Team Blue": [lynn]
-}
+ruffus = Puppy()
+print(ruffus.name, ruffus.age, ruffus.breed)
