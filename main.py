c2c40f14aa97997fbfe7b74328e135991eb0b0b5
bioroid17 <trace1997@ajou.ac.kr>
2024-07-01 17:56:37 +0900

    OOP - Code Challenge
    remove player from team & get total xp from team

--- main.py
+++ main.py
@@ -1,33 +1,53 @@
-class Dog:
-    def __init__(self, name, breed, age):
+class Player:
+    def __init__(self, name, team):
         self.name = name
-        self.breed = breed
-        self.age = age
+        self.xp = 1500
+        self.team = team
 
-    def sleep(self):
-        print("zzzzzzzzz...sleeping")
-
-
-class GuardDog(Dog):
-    def __init__(self, name, breed):
-        super().__init__(name, breed, 5)
-        self.aggressive = True
-
-    def rrrr(self):
-        print("Stay away!")
+    def introduce(self):
+        print(f"Hello! I'm {self.name} and I play for {self.team}")
 
 
-class Puppy(Dog):
-    def __init__(self, name, breed):
-        super().__init__(name, breed, 0.1)
-        self.spoiled = True
+class Team:
+    def __init__(self, team_name):
+        self.name = team_name
+        self.players = []
 
-    def woof_woof(self):
-        print("Woof Woof")
+    def show_players(self):
+        for player in self.players:
+            player.introduce()
+
+    def add_player(self, name):
+        new_player = Player(name, self.name)
+        self.players.append(new_player)
+
+    def delete_player(self, name):
+        for player in self.players:
+            if name == player.name:
+                self.players.remove(player)
+                print(
+                    f"Player with the name of {name} is removed from {self.name}")
+                return
+        else:
+            print(f"Player with the name of {name} doesn't exist")
+            return
+
+    def show_total_xp(self):
+        total = 0
+        for player in self.players:
+            total += player.xp
+        print(f"The sum of our team's xp is {total}")
 
 
-ruffus = Puppy(name="Ruffus", breed="Beagle")
-bibi = GuardDog(name="Bibi", breed="Dalmatian")
+team_x = Team("Team X")
+team_x.add_player("Nico")
+team_blue = Team("Team Blue")
 
-ruffus.sleep()
-bibi.sleep()
+team_blue.add_player("Lynn")
+team_blue.add_player("Lee")
+
+team_blue.show_players()
+
+team_blue.delete_player("l")
+
+team_blue.show_total_xp()
