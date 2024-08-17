cc779f5db986e35e8da2bfafdef656d5d7d051ac
bioroid17 <trace1997@ajou.ac.kr>
2024-08-18 05:31:35 +0900

    JOB SCRAPER - Pagination

--- main.py
+++ main.py
@@ -1,40 +1,50 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://weworkremotely.com/categories/remote-full-stack-programming-jobs"
-
-response = requests.get(url)
-
-soup = BeautifulSoup(
-    response.content,
-    "html.parser",
-)
-
-jobs = soup.find("section", class_="jobs").find_all("li")[1:-1]
-
 all_jobs = []
 
-for job in jobs:
-    title = job.find("span", class_="title").text
-    try:
-        region = job.find("span", class_="region").text
-    except AttributeError:
-        region = "No Region"
 
-    try:
-        company, position, _ = job.find_all("span", class_="company")
-    except ValueError:
-        company, position = job.find_all("span", class_="company")
-    company = company.text
-    position = position.text
-    url = job.find("div", class_="tooltip--flag-logo").next_sibling["href"]
-    job_data = {
-        "title": title,
-        "company": company,
-        "position": position,
-        "region": region,
-        "url": f"https://weworkremotely.com{url}",
-    }
-    all_jobs.append(job_data)
+def scrape_page(url):
+    print(f"Scrapping {url}")
+    response = requests.get(url)
+    soup = BeautifulSoup(
+        response.content,
+        "html.parser",
+    )
+    jobs = soup.find("section", class_="jobs").find_all("li")[1:-1]
+    for job in jobs:
+        title = job.find("span", class_="title").text
+        try:
+            region = job.find("span", class_="region").text
+        except AttributeError:
+            region = "No Region"
 
-print(all_jobs)
+        try:
+            company, position, _ = job.find_all("span", class_="company")
+        except ValueError:
+            company, position = job.find_all("span", class_="company")
+        company = company.text
+        position = position.text
+        url = job.find("div", class_="tooltip--flag-logo").next_sibling["href"]
+        job_data = {
+            "title": title,
+            "company": company,
+            "position": position,
+            "region": region,
+            "url": f"https://weworkremotely.com{url}",
+        }
+        all_jobs.append(job_data)
+
+
+def get_pages(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    return len(soup.find("div", class_="pagination").find_all("span", class_="page"))
+
+
+total_pages = get_pages("https://weworkremotely.com/remote-full-time-jobs?page=1")
+for x in range(total_pages):
+    url = f"https://weworkremotely.com/remote-full-time-jobs?page={x+1}"
+    scrape_page(url)
+
+print(len(all_jobs))
