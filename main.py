2e7c82d8eeb4912bfc37db610690a72896a5f179
bioroid17 <trace1997@ajou.ac.kr>
2024-08-17 22:20:17 +0900

    JOB SCRAPER - Jobs

--- main.py
+++ main.py
@@ -10,5 +10,20 @@ soup = BeautifulSoup(
     "html.parser",
 )
 
-jobs = soup.find("section", class_="jobs").find_all("li")
-print(jobs)
+jobs = soup.find("section", class_="jobs").find_all("li")[1:-1]
+
+for job in jobs:
+    title = job.find("span", class_="title").text
+    try:
+        region = job.find("span", class_="region").text
+    except AttributeError:
+        region = "No Region"
+
+    try:
+        company, position, _ = job.find_all("span", class_="company")
+    except ValueError:
+        company, position = job.find_all("span", class_="company")
+
+    company = company.text
+    position = position.text
+    print(title, company, position, region, "----------\n")
