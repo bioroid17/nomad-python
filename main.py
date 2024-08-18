c421d6819067fcf22fc5d95c9b6ba84fafa24de9
bioroid17 <trace1997@ajou.ac.kr>
2024-08-18 21:50:45 +0900

    JOB SCRAPER - Playwright

--- main.py
+++ main.py
@@ -1,47 +1,11 @@
-import requests
-from bs4 import BeautifulSoup
+from playwright.sync_api import sync_playwright
 
+p = sync_playwright().start()
 
-class Job:
-    def __init__(self, url, company, location, salary) -> None:
-        self.url = url
-        self.company = company
-        self.location = location
-        self.salary = salary
+browser = p.chromium.launch(headless=False)
 
-    def __str__(self) -> str:
-        return f"URL: {self.url}\nCompany: {self.company}\nLocation: {self.location}\nSalary:{self.salary}\n"
+page = browser.new_page()
 
+page.goto("https://google.com")
 
-keywords = ["flutter", "python", "golang"]
-all_jobs = []
-for keyword in keywords:
-    response = requests.get(
-        f"https://remoteok.com/remote-{keyword}-jobs",
-        headers={
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36"
-        },
-    )
-    soup = BeautifulSoup(response.content, "html.parser")
-    jobs = soup.find_all("tr", class_="job")
-
-    keyword_jobs = []
-    for job in jobs:
-        url = f"https://remoteok.com{job['data-url']}"
-        company = job["data-company"]
-        try:
-            location = job.find("div", class_="tooltip").text
-        except AttributeError:
-            location = "Unknown"
-        try:
-            salary = job.find("div", class_="tooltip-set").text
-        except AttributeError:
-            salary = "Unknown"
-        keyword_jobs.append(Job(url, company, location, salary))
-    all_jobs.append({keyword: keyword_jobs})
-
-for d in all_jobs:
-    for key, value in d.items():
-        print(f"================={key}=================")
-        for job in value:
-            print(job)
+page.screenshot(path="screenshot.png")
