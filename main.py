96a0857a33a2acf4550caedb0747603d32f46d6e
bioroid17 <trace1997@ajou.ac.kr>
2024-08-19 23:24:50 +0900

    JOB SCRAPER - Code Challenge
    Code Challenge: All in function (+ to OOP)

--- main.py
+++ main.py
@@ -3,63 +3,90 @@ import time
 from bs4 import BeautifulSoup
 import csv
 
-p = sync_playwright().start()
 
-browser = p.chromium.launch(headless=False)
+class WantedScraper:
+    def __init__(self, keyword):
+        self.jobs_db = []
+        self.keyword = keyword
 
-page = browser.new_page()
+    def get_content(self):
+        p = sync_playwright().start()
 
-page.goto("https://www.wanted.co.kr/search?query=flutter&tab=position")
-# time.sleep(5)
+        browser = p.chromium.launch(headless=False)
 
-# page.click("button.Aside_searchButton__rajGo")
-# time.sleep(5)
+        page = browser.new_page()
 
-# page.get_by_placeholder("검색어를 입력해 주세요.").fill("flutter")
-# time.sleep(5)
+        page.goto(f"https://www.wanted.co.kr/search?query={self.keyword}&tab=position")
+        # time.sleep(5)
 
-# page.keyboard.down("Enter")
-# time.sleep(5)
+        # page.click("button.Aside_searchButton__rajGo")
+        # time.sleep(5)
 
-# page.click("a#search_tab_position")
+        # page.get_by_placeholder("검색어를 입력해 주세요.").fill("flutter")
+        # time.sleep(5)
 
-for x in range(5):
-    time.sleep(5)
-    page.keyboard.down("End")
+        # page.keyboard.down("Enter")
+        # time.sleep(5)
 
-content = page.content()
+        # page.click("a#search_tab_position")
 
-p.stop()
+        for x in range(5):
+            time.sleep(5)
+            page.keyboard.down("End")
 
-soup = BeautifulSoup(content, "html.parser")
+        content = page.content()
 
-jobs = soup.find_all("div", class_="JobCard_container__REty8")
+        p.stop()
 
-jobs_db = []
+        return content
 
-for job in jobs:
-    link = f"https://www.wanted.co.kr{job.find('a')['href']}"
-    title = job.find("strong", class_="JobCard_title__HBpZf").text
-    company_name = job.find("span", class_="JobCard_companyName__N1YrF").text
-    reward = job.find("span", class_="JobCard_reward__cNlG5").text
+    def scrape(self):
 
-    j = {
-        "title": title,
-        "company_name": company_name,
-        "reward": reward,
-        "link": link,
-    }
-    jobs_db.append(j)
+        content = self.get_content()
 
-file = open("jobs.csv", "w", encoding="utf-8-sig")
-writer = csv.writer(file)
-writer.writerow(
-    [
-        "Title",
-        "Company",
-        "Reward",
-        "Link",
-    ]
-)
-for job in jobs_db:
-    writer.writerow(job.values())
+        soup = BeautifulSoup(content, "html.parser")
+
+        jobs = soup.find_all("div", class_="JobCard_container__REty8")
+
+        jobs_db = []
+
+        for job in jobs:
+            link = f"https://www.wanted.co.kr{job.find('a')['href']}"
+            title = job.find("strong", class_="JobCard_title__HBpZf").text
+            company_name = job.find("span", class_="JobCard_companyName__N1YrF").text
+            reward = job.find("span", class_="JobCard_reward__cNlG5").text
+
+            j = {
+                "title": title,
+                "company_name": company_name,
+                "reward": reward,
+                "link": link,
+            }
+            jobs_db.append(j)
+
+        file = open(f"{self.keyword}_jobs.csv", "w", encoding="utf-8-sig")
+        writer = csv.writer(file)
+        writer.writerow(
+            [
+                "Title",
+                "Company",
+                "Reward",
+                "Link",
+            ]
+        )
+        for job in jobs_db:
+            writer.writerow(job.values())
+        file.close()
+
+
+keywords = [
+    "python",
+    "django",
+    "nextjs",
+    "flutter",
+    "kotlin",
+]
+
+for keyword in keywords:
+    scraper = WantedScraper(keyword)
+    scraper.scrape()
